7afa204c327d3162ce847fd6fc319252d96161fa
Akash Kute <kuteakash51@gmail.com>
2025-06-30 22:20:54 -0400

    feat: base-setup

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <raylib.h>
+
+using namespace std;
+
+int main () {
+
+   cout<<"starting the game..."<<endl;
+   InitWindow(750, 750, "Retro Snake");
+
+   CloseWindow();
+   return 0;
+
+}
